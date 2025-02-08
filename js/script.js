f4cbf39af68333be82f60b3ee1eda02381452df2
Bona Hirpa <bonaforg2@gmail.com>
2025-02-08 10:50:17 +0300

    i added header and home section

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Header part
+// Toggle mobile menu
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.querySelector(".nav-links").classList.toggle("active");
+});
